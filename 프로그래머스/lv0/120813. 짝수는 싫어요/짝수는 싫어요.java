0f5d9f27178f1cb9cc2438a930006af5a124aad5
서라 <93528672+seolha86@users.noreply.github.com>
2023-07-24 11:52:03 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 70.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int n) {
+        int cnt = 0;
+        int[] answer = new int[n % 2 == 0 ? n / 2 : n / 2 + 1];
+        
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                answer[cnt] = i;
+                cnt++;
+            }
+        }
+        
+        return answer;
+    }
+}
