8b947a2a42996a4baf88f685f82b8212469edb31
서라 <93528672+seolha86@users.noreply.github.com>
2023-07-27 20:22:53 +0900

    [level 0] Title: 모음 제거, Time: 2.19 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+    public String solution(String my_string) {
+        return Arrays.stream(my_string.split("")).filter(s -> (!s.equals("a") && !s.equals("e") && !s.equals("i") && !s.equals("o") && !s.equals("u"))).collect(Collectors.joining());
+    }
+}
